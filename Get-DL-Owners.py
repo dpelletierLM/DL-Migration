bafde18a809aab06ad76d3c2fe4a6948823c61f4
DONALD PELLETIER <Donald.Pelletier@LibertyMutual.com>
2022-09-06 15:31:00 -0400

    This version getting the access token for graph api is successful

--- Get-DL-Owners.py
+++ Get-DL-Owners.py
@@ -1,19 +1,53 @@
 #!/usr/bin/env python3
-
-#import libraries
-import requests
+from pprint import pprint
+from requests import post
 import json
-from microsoftgraph.client import Client
+from datetime import datetime, date
+
+from_date = date.today()
+to_date = date.today()
+timenow = datetime.now()
+current_time = timenow.strftime("%H:%M:%S")
 
 clientId = "ed629446-39a5-4c06-90e5-266f636444b6"
 clientSecret = ".A~LjDJHl01MaTGeEhQKdK8A~U_pEpz997"
 tenantId = "08a83339-90e7-49bf-9075-957ccd561bf1"
-#authTenant = common
-#graphUserScopes = User.Read Mail.Read Mail.Send
-scope = "https%3A%2F%2Fgraph.microsoft.com%2F.default"
+scopeInfo = "https%3A%2F%2Fgraph.microsoft.com%2F.default"
 redirectURL = "https://graph.microsoft.com"
 
-client = Client('CLIENT_ID', 'CLIENT_SECRET', account_type='common') # by default common, thus account_type is optional parameter.
+def get_graph_access_token():
+    """Returns the access token needed to use API calls with MS Graph"""
 
-url = client.authorization_url("https://graph.microsoft.com", scope, state=None)
-response = client.exchange_code(redirectURL, code)
+    token_URL = f'https://login.microsoftonline.com/{tenantId}/oauth2/v2.0/token'
+
+    headers = {
+        'Host': 'login.microsoftonline.com',
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+
+    body = {
+        'client_id': clientId,
+        'scope': 'https://graph.microsoft.com/.default',
+        'client_secret': clientSecret,
+        'grant_type': 'client_credentials',
+        'tenant': tenantId
+    }
+
+
+    response = post(url=token_URL, headers=headers, data=body)
+    response.raise_for_status()
+    response_body = response.json()
+    #response_object = json.loads(response_body)
+    json_formmatted_str = json.dumps(response_body, indent=0)
+    #print(response_body)
+    print(json_formmatted_str)
+    authorization_token = f"{response_body['token_type']} {response_body['access_token']}"
+
+    print(authorization_token)
+
+get_graph_access_token()
+
+def get_groups():
+    """Gets all groups in AD in Azure"""
+    
