c46694646dbed676c623e7162f928ee74498d67a
DONALD PELLETIER <Donald.Pelletier@LibertyMutual.com>
2022-09-06 13:06:24 -0400

    modified:   Get-DL-Owners.py

--- Get-DL-Owners.py
+++ Get-DL-Owners.py
@@ -10,8 +10,10 @@ clientSecret = ".A~LjDJHl01MaTGeEhQKdK8A~U_pEpz997"
 tenantId = "08a83339-90e7-49bf-9075-957ccd561bf1"
 #authTenant = common
 #graphUserScopes = User.Read Mail.Read Mail.Send
+scope = "https%3A%2F%2Fgraph.microsoft.com%2F.default"
+redirectURL = "https://graph.microsoft.com"
 
 client = Client('CLIENT_ID', 'CLIENT_SECRET', account_type='common') # by default common, thus account_type is optional parameter.
 
 url = client.authorization_url("https://graph.microsoft.com", scope, state=None)
-
+response = client.exchange_code(redirectURL, code)
