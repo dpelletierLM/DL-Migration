99cdf94627e1c99afa080aa1205115896e8b6708
DONALD PELLETIER <Donald.Pelletier@LibertyMutual.com>
2022-09-06 16:25:41 -0400

    Script is getting groups and their IDs.  Not sure it is getting all of them though.

--- Get-DL-Owners.py
+++ Get-DL-Owners.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from pprint import pprint
-from requests import post
+import requests
 import json
 from datetime import datetime, date
 
@@ -13,7 +13,7 @@ clientId = "ed629446-39a5-4c06-90e5-266f636444b6"
 clientSecret = ".A~LjDJHl01MaTGeEhQKdK8A~U_pEpz997"
 tenantId = "08a83339-90e7-49bf-9075-957ccd561bf1"
 scopeInfo = "https%3A%2F%2Fgraph.microsoft.com%2F.default"
-redirectURL = "https://graph.microsoft.com"
+graph_URL = "https://graph.microsoft.com"
 
 def get_graph_access_token():
     """Returns the access token needed to use API calls with MS Graph"""
@@ -35,19 +35,41 @@ def get_graph_access_token():
     }
 
 
-    response = post(url=token_URL, headers=headers, data=body)
+    response = requests.post(url=token_URL, headers=headers, data=body)
     response.raise_for_status()
     response_body = response.json()
     #response_object = json.loads(response_body)
     json_formmatted_str = json.dumps(response_body, indent=0)
     #print(response_body)
-    print(json_formmatted_str)
+    #print(json_formmatted_str)
     authorization_token = f"{response_body['token_type']} {response_body['access_token']}"
+    #print(authorization_token)
+    return authorization_token # returns the authorization token value from the function block to the authorization token variable outside of this function so it can be
 
-    print(authorization_token)
+authorization_token = get_graph_access_token() # Sets authorization token variable to the value returned from get graph access token function
 
-get_graph_access_token()
 
-def get_groups():
+graph_URL = "https://graph.microsoft.com"
+
+def get_groups(authorization_token):
     """Gets all groups in AD in Azure"""
+
+    get_groups_URL = f'{graph_URL}/v1.0/groups'
     
+    get_groups_headers = {
+        'Authorization': authorization_token
+    }
+
+    get_groups_body = {
+    }
+
+    get_groups_response = requests.get(url=get_groups_URL, headers=get_groups_headers, data=get_groups_body)
+    get_groups_response.raise_for_status()
+    get_groups_response_body = get_groups_response.json()
+    group_ids = get_groups_response_body['value'] 
+    for group in group_ids:
+        print(group['id'])
+
+    #print(group_ids)
+
+get_groups(authorization_token)
